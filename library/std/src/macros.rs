7f00f3486d1ddb4ee1eff5930f3ab762590540f4
stifskere <esteve@memw.es>
2025-07-17 16:04:18 +0200

    fix: remove duplicate hash_map_macro feature

--- library/std/src/macros.rs
+++ library/std/src/macros.rs
@@ -414,7 +414,6 @@ macro_rules! dbg {
 #[macro_export]
 #[allow_internal_unstable(hint_must_use)]
 #[unstable(feature = "hash_map_macro", issue = "144032")]
-#[rustc_diagnostic_item = "hash_map_macro"]
 macro_rules! hash_map {
     () => {{
         ::std::collections::HashMap::new()
